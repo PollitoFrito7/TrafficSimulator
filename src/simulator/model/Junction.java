e937f898c08330111b10a1a55a5fee393559f3e4
alyssa chioldin <achioldi@ucm.es>
2025-02-06 18:08:17 +0100

    patata

--- src/simulator/model/Junction.java
+++ src/simulator/model/Junction.java
@@ -1,5 +1,55 @@
 package simulator.model;
 
-public class Junction {
+import java.util.List;
+import java.util.Map;
 
+import org.json.JSONObject;
+
+public class Junction extends SimulatedObject {
+	private List<Road> _inRoads;
+	private List<List<Vehicle>> _queues;
+	private Map<Road, List<Vehicle>> _queueByRoad;
+	private Map<Junction, Road> _outRoadByJunction;
+	private int _greenLightIndex;
+	private int _lastSwitchingTime;
+	private LightSwitchingStrategy _lss;
+	private DequeuingStrategy _dqs;
+	
+	Junction(String id, LightSwitchingStrategy lsStrategy, DequeuingStrategy dqStrategy) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+	
+	protected void addIncommingRoad(Road r) {
+		
+	}
+	
+	protected void addOutgoingRoad(Road r) {
+		
+	}
+	
+	protected void enter(Vehicle v) {
+		
+	}
+	
+	protected Road roadTo(Junction j) {
+		return null;
+	}
+	
+	@Override
+	protected void advance(int time) {
+		
+	}
+
+	@Override
+	public JSONObject report() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/*
+	@Override
+	public void JSONObject report() {
+		return null;
+	}*/
 }
--- src/simulator/model/Vehicle.java
+++ src/simulator/model/Vehicle.java
@@ -16,74 +16,89 @@ public class Vehicle extends SimulatedObject {
 	private int _contaminationClass = 0;
 	private int _totalContamination = 0;
 	private int _totalTraveledDist = 0;
+	private int _lastJunctionEncountered = 0;
 
 	protected Vehicle(String id, int maxSpeed, int contClass, List<Junction> itinerary) {
 		super(id);
-		
-		if(id.isEmpty()) throw new IllegalArgumentException("The string cannot be empty.");
+
+		if (id.isEmpty())
+			throw new IllegalArgumentException("The string cannot be empty.");
 		else _id = id;
-		if(maxSpeed <= 0) throw new IllegalArgumentException("The maximum speed must be positive.");
+		if (maxSpeed <= 0) throw new IllegalArgumentException("The maximum speed must be positive.");
 		else _maximumSpeed = maxSpeed;
-		if(0 > contClass || contClass > 10) throw new IllegalArgumentException("The contamination class must be a number between 0 and 10 (both inclusive");
+		if (0 > contClass || contClass > 10)
+			throw new IllegalArgumentException("The contamination class must be a number between 0 and 10 (both inclusive");
 		else setContaminationClass(contClass);
-		if(itinerary == null || itinerary.size() != 2) throw new IllegalArgumentException("The length of the list must be at least 2");
+		if (itinerary == null || itinerary.size() != 2)
+			throw new IllegalArgumentException("The length of the list must be at least 2");
 		else setItinerary(Collections.unmodifiableList(new ArrayList<>(itinerary)));
 	}
-	
 
 	protected void setSpeed(int s) {
-		if(s < 0) throw new IllegalArgumentException("The speed cannot be negative.");
-		else _currentSpeed = Math.min(s, _maximumSpeed);
-		
+		if (s < 0)
+			throw new IllegalArgumentException("The speed cannot be negative.");
+		else
+			_currentSpeed = Math.min(s, _maximumSpeed);
+
 	}
-	
+
 	protected void setContaminationClass(int c) {
-		if(c < 0 || c > 10) throw new IllegalArgumentException("The contamination cannot be set properly.");
-		else _contaminationClass = c;
+		if (c < 0 || c > 10)
+			throw new IllegalArgumentException("The contamination cannot be set properly.");
+		else
+			_contaminationClass += c;
 		// setContClass does the same as above in the else statement
 	}
-	
 
 	@Override
 	protected void advance(int time) {
-		while (_status == VehicleStatus.TRAVELING) {
-			int _newLocation = 0;
-			_newLocation = Math.min(_location + _currentSpeed, _road.getLength());
-			
-			if(_newLocation> _road.getLength()) {
-				_newLocation = _road.getLength();
-			}
-			
-			int d = _totalTraveledDist;
-			_totalTraveledDist = _newLocation - _location;
-			
-			int f = _contaminationClass;
-			int c = d * f;
-			
-			_totalContamination += c;
-			_road.addContamination(f);
-			
-			if(_newLocation == _road.getLength()) {
-				// enter function of Junction class
-				_status = VehicleStatus.PENDING;
-				_currentSpeed = 0;
-//			It is recommended to keep track of the index of the last junction encountered
-//			This starts at 0 and is incremented by 1 when entering a junction's queue
-				
-			}
-			
-			
-			
+		if (_status != VehicleStatus.TRAVELING)
+			return;
+
+//			maybe put some of this in separated functions?¿
+		int _newLocation = 0;
+		_newLocation = Math.min(_location + _currentSpeed, _road.getLength());
+
+		int d = _totalTraveledDist;
+		_totalTraveledDist = _newLocation - _location;
+
+		int f = _contaminationClass;
+		int c = d * f;
+
+		_totalContamination += c;
+		_road.addContamination(f);
+
+		if (_newLocation == _road.getLength()) {
+			_itinerary.get(_lastJunctionEncountered).enter(this);
+			_status = VehicleStatus.PENDING;
+			_currentSpeed = 0;
+			_lastJunctionEncountered++;
+
 		}
 	}
 
 	protected void moveToNextRoad() {
-		if(_status == VehicleStatus.ARRIVED || _status == VehicleStatus.TRAVELING) throw new IllegalArgumentException("The status must be pending or waiting.");
+		if (_status != VehicleStatus.PENDING || _status != VehicleStatus.WAITING)
+			throw new IllegalArgumentException("The status must be pending or waiting.");
+		else if (_road != null || _lastJunctionEncountered > 0) {
+			_road.exit(this);
+		}
+		else if(_status == VehicleStatus.WAITING && _itinerary.size() == _lastJunctionEncountered) {			
+			_status = VehicleStatus.ARRIVED;
+			_currentSpeed = 0;
+			_road = null;
+		} else {
+			_itinerary.get(_lastJunctionEncountered).roadTo();
+	
+			_road.enter(this);
+			_status = VehicleStatus.TRAVELING;
+			_location = 0;
+		}
 		
+
 	}
 
-	
-	// see which type of modifier suits better with getters and setters 
+	// see which type of modifier suits better with getters and setters
 	public int getLocation() {
 		return _location;
 	}
@@ -91,19 +106,19 @@ public class Vehicle extends SimulatedObject {
 	public int getCurrentSpeed() {
 		return _currentSpeed;
 	}
-	
+
 	public int getContClass() {
 		return _contaminationClass;
 	}
-	
+
 	public int getMaxSpeed() {
 		return _maximumSpeed;
 	}
-	
+
 	public VehicleStatus getStatus() {
 		return _status;
 	}
-	
+
 	public int getTotalCO2() {
 		return _totalContamination;
 	}
@@ -111,42 +126,30 @@ public class Vehicle extends SimulatedObject {
 	public List<Junction> getItinerary() {
 		return _itinerary;
 	}
-	
+
 	public Road getRoad() {
 		return _road;
 	}
-	
-	
+
 	// additional setters declared as private
-	
-	// this method is equivalent to say _contaminationClass = c in the method setContaminationClass
-	private void setContClass(int contaminationClass) {
-		_contaminationClass = contaminationClass;
-	}
-	
+
+	// this method is equivalent to say _contaminationClass = c in the method
+	// setContaminationClass
+	/*
+	 * private void setContClass(int contaminationClass) { _contaminationClass =
+	 * contaminationClass; }
+	 * 
+	 */
+
 	private void setItinerary(List<Junction> itinerary) {
 		_itinerary = itinerary;
 	}
-	
-	
-	
-	
-	
-	
-	/*
-	 * @Override
-	public JSONObject report() {
-		JSONObject o = new JSONObject();
-		
-		return o.put("patata", "yes");
-	}
-	
-	 * 
-	 * 
-	 * */
- 	
-	
 
-	
+	@Override
+	public JSONObject report() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
