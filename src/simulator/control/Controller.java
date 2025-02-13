b53b12f31704d95e007d7144b2b1ff0737014d80
PollitoFrito7 <jdelrod04@gmail.com>
2025-02-13 17:07:47 +0100

    Controller implemented
    not tested

---
+++ src/simulator/control/Controller.java
@@ -0,0 +1,58 @@
+package simulator.control;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import simulator.factories.Factory;
+import simulator.model.Event;
+import simulator.model.TrafficSimulator;
+
+public class Controller {
+	private TrafficSimulator _sim;
+	private Factory<Event> _eventsFactory;
+	
+	public Controller(TrafficSimulator sim, Factory<Event> eventsFactory) {
+		_sim = sim;
+		_eventsFactory = eventsFactory;
+	}
+	
+	public void loadEvents(InputStream in) {
+		JSONObject jo = new JSONObject(new JSONTokener(in));
+		JSONArray events = jo.getJSONArray("events");
+		for (int i = 0; i <  events.length(); i++) {
+			_sim.addEvent(_eventsFactory.create_instance(events.getJSONObject(i)));
+		}
+	}
+	
+	public void run(int n, OutputStream out) {
+		PrintStream p = new PrintStream(out);
+		
+		// print "{" to 'p'
+		// print "  \"states\": [" to 'p'
+
+		// loop for the first n-1 states (to print comma after each state)
+		for (int i = 0; i < n - 1; i++) {
+		    _sim.advance();
+		    p.print(_sim.report());
+		    p.println(",");
+		}
+
+		// last step, only if 'n > 0'
+		if (n > 0) {
+		    _sim.advance();
+		    p.print(_sim.report());
+		}
+
+		// print "]" to 'p'
+		// print "}" to 'p'
+	}
+	
+	public void reset() {
+		_sim.reset();
+	}
+}
